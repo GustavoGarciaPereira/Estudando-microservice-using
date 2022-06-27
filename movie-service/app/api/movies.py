1dc8605a8d2cd51ef916ae55a687da4f23f02574
gustavo <gusgurtavo@gmail.com>
2022-06-27 20:51:56 -0300

    setup postgressql

--- movie-service/app/api/movies.py
+++ movie-service/app/api/movies.py
@@ -1,18 +1,20 @@
 
 from typing import List
-from fastapi import Header, APIRouter
+from urllib import response
+from fastapi import Header, APIRouter, HTTPException
 
 
-from app.api.models import Movie
+from app.api.models import MovieIn, MovieOut
+from app.api import db_manager
 
-fake_movie_db = [
-    {
-        'name': 'Star Wars: Episode IX - The Rise of Skywalker',
-        'plot': 'The surviving members of the resistance face yhr First',
-        'genres': ['Action', 'Adventure', 'Fantasy'],
-        'casts': ['Daisy Ridley', 'Adam Driver']
-    }
-]
+# fake_movie_db = [
+#     {
+#         'name': 'Star Wars: Episode IX - The Rise of Skywalker',
+#         'plot': 'The surviving members of the resistance face yhr First',
+#         'genres': ['Action', 'Adventure', 'Fantasy'],
+#         'casts': ['Daisy Ridley', 'Adam Driver']
+#     }
+# ]
 
 
 movies = APIRouter()
@@ -20,32 +22,40 @@ movies = APIRouter()
 
 @movies.get('/', response_model=List[Movie])
 async def index():
-    return fake_movie_db
+    return await db_maneger.get_all_movies()
 
 
 @movies.post('/', status_code=201)
-async def add_movie(payload: Movie):
-    movie = payload.dict()
-    fake_movie_db.append(movie)
-    return {'id': len(fake_movie_db)-1}
+async def add_movie(payload: MovieIn):
+    movie_id = await db_maneger.add_movie(payload)
+    response = {
+        'id': movie_id,
+        **payload.dict()
+    }
+    return response
 
 
 @movies.put('/{id}')
-async def update_movie(id: int, payload: Movie):
-    movie = payload.dict()
-    movies_length = len(fake_movie_db)
-    if 0 <= id <= movies_length:
-        fake_movie_db[id] = movie
-        return None
-    raise HTTPException(status_code=404,
-                        detail="Movie with given id not found")
+async def update_movie(id: int, payload: MovieIn):
+    movie = await db_maneger.db_maneger(id)
+    if not movie:
+        raise HTTPException(
+            status_code=404,
+            datail="Movie not found"
+        )
 
+    update_data = payload.dict(exclude_unset=True)
+    movie_in_db = MovieIn(**movie)
+
+    updated_movie = movie_in_db.copy(update=update_data)
+    return await db_maneger.update_movie(id, updated_movie)
 
 @movies.delete('/{id}')
 async def delete_movie(id: int):
-    movies_length = len(fake_movie_db)
-    if 0 <= id <= movies_length:
-        del fake_movie_db[id]
-        return None
-    raise HTTPException(status_code=404,
-                        detail="Movie with given id not found")
+    movie = await db_maneger.get_movie(id)
+    if not movie:
+        raise HTTPException(
+            status_code=404,
+            datail="Movie not found"
+        )
+    return await db_maneger.delete_movie(id)
--- movie-service/app/main.py
+++ movie-service/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
-
 from app.api.movies import movies
+from app.api.db import metadata, database, engine
 
+metadata.create_all(engine)
 
 app = FastAPI()
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdpwn")
+async def shutdpwn():
+    await database.disconnect()
+
 
 app.include_router(movies)
